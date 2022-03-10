cc085d1e16abc4f1e5094e5b0d4a5ab09995bb65
Andrew Gary <andrew.gary91@gmail.com>
2022-03-10 11:42:56 -0600

    created start script for heroku

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const server = express();
 
-
+//npx kill-port 9000
+//npx gitignore node
+//npx eslint --init
 
 server.get('/', (req, res) => {
-    res.end('<h1>Helloooo world</h1>')
+    res.end('<h1>Helloooo world</h1')
 })
 
 server.listen(9000, () => {
